e79d3cab12b19dc357a1ea70be33642c21be2fe4
bhkstore0-source <bhk.store0@gmail.com>
2026-04-29 10:21:42 +0200

    Update SCRIPT_URL to new Google Script URL

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 const PRODUCT_PRICE      = 2000;
 const PRODUCT_NAME       = 'جهاز شد الرقبة و الوجه';
-const SCRIPT_URL         = 'https://script.google.com/macros/s/AKfycbygf6UJ-H_ujE7S7cj6rd2sSQB9Oy5gkRZg0zbE9Xbm-hs-wyc6c5vO2rv1Ls2loUIaEw/exec';
+const SCRIPT_URL         = 'https://script.google.com/macros/s/AKfycbyoQBV3aEVZraNp8WgvMhHEdmVXGKIvH8Gor5i9bDoFUladSqrKNWZIudkdY54wGUq6bA/exec';
 const WHATSAPP_NUM       = '213553096569';
 const RESTRICTED_WILAYAS = ['52', '56', '57'];
 
